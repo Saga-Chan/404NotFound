43d81388bcf72a16120f3044ab8d6f3c64628228
MaxenceDecanter <maxence.decanter@hotmail.fr>
2018-03-19 09:49:36 +0100

    Projet avec map custom

---
+++ ProjetJava/core/assets/tile.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tile" tilewidth="16" tileheight="16" tilecount="40" columns="8">
+ <image source="../../../../../grotto_escape_pack/grotto_escape_pack/graphics/tiles.png" width="128" height="80"/>
+</tileset>
